5752dd539988d42fe244ffdfa535590299d4976b
Zakhar Zibarov <zakhar.zibarov@gmail.com>
2010-10-05 15:59:33 +0400

    added readme and long description

---
+++ README.txt
@@ -0,0 +1,11 @@
+Installation
+------------
+
+In Ubuntu you can install the above using the following commands:
+
+sudo aptitude install ffmpeg cython
+
+tar -xf FFVideo-0.0.7.tar.gz
+cd FFVideo-0.0.7
+python setup.py install
+
--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+import os
 from distutils.core import setup, Extension
 from Cython.Distutils import build_ext
 
+def read(fn):
+    return open(os.path.join(os.path.dirname(__file__), fn)).read()
+
+VERSION = "0.0.7"
+
 setup(
     name="FFVideo",
-    version="0.0.7",
+    version=VERSION,
     description="FFVideo is a python extension makes possible to access to decoded frames at two format: PIL.Image or numpy.ndarray.",
+    long_description=read("README.txt"),
 
     ext_modules=[
         Extension("ffvideo", ["ffvideo/ffvideo.pyx"],
