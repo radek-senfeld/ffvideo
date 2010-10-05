62068bb1f009b059b98698058873e1c166029890
Zakhar Zibarov <zakhar.zibarov@gmail.com>
2010-10-05 15:45:27 +0400

    Added description to setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,19 @@ from Cython.Distutils import build_ext
 
 setup(
     name="FFVideo",
+    version="0.0.7",
+    description="FFVideo is a python extension makes possible to access to decoded frames at two format: PIL.Image or numpy.ndarray.",
+
     ext_modules=[
         Extension("ffvideo", ["ffvideo/ffvideo.pyx"],
         include_dirs=["/usr/include/ffmpeg"],
         libraries=["avformat", "avcodec", "swscale"])
     ],
     cmdclass={'build_ext': build_ext},
-    version="0.0.6dev3",
 #    test_suite='nose.collector',
 #    tests_require=['nose'],
     author="Zakhar Zibarov",
     author_email="zakhar.zibarov@gmail.com",
     url="http://bitbucket.org/zakhar/ffvideo/",
 )
+
